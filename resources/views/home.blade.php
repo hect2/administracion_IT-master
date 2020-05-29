778a8f9a0d2e8b68f48a3f3d88b0eeba9f7e06d3
obed5 <hxocop@homeland.com.gt>
2020-05-29 16:28:10 -0600

    **cambio**

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">KPI</div>
+                <div class="card-header">dash</div>
 
                 <div class="card-body">
                     @if (session('status'))
